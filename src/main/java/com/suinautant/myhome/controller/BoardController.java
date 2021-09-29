8a2d735da08c910c45a2ce89673ea2b50d3b97d6
suinautant <suinautant@gmail.com>
2021-09-29 22:55:42 +0900

    func page

--- src/main/java/com/suinautant/myhome/controller/BoardController.java
+++ src/main/java/com/suinautant/myhome/controller/BoardController.java
@@ -5,6 +5,10 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -24,13 +28,22 @@ public class BoardController {
 
 	@Autowired
 	private BoardRepository boardRepository;
-	
+
 	@Autowired
 	private BoardValidator boardValidator;
 
+	// Pageable 설정시 req 매개변수 page와 size 사용 가능
 	@GetMapping("/list")
-	public String list(Model model) {
-		List<Board> boards = boardRepository.findAll();
+	public String list(Model model, @PageableDefault(size = 2) Pageable pageable) {
+		Page<Board> boards = boardRepository.findAll(pageable);
+		// boards.getPageable().getPageNumber() : 현재 페이지 번호
+		// boards.getTotalPages()  : 전체 페이지 수
+		// ?? 4를 빼고 더하는 과정이 필요한가 ??
+		int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+		int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+
+		model.addAttribute("startPage", startPage);
+		model.addAttribute("endPage", endPage);
 		model.addAttribute("boards", boards);
 		return "board/list";
 	}
